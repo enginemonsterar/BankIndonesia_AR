e1635de90d42dc817364527382969e9dc2785b3a
Januar Elsan <januarelsan@gmail.com>
2019-09-25 20:58:47 +0700

    Create Audio Manager

---
+++ Assets/EasyAR/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioListener audioListener;
+
+    void Awake()
+    {
+        
+    }
+    public void ChangeAudioActiveMode(){
+        if(PrefsManager.Instance.AudioActiveMode == 1){
+            PrefsManager.Instance.AudioActiveMode = 0;
+            audioListener.enabled = false;
+        }else
+        {
+            PrefsManager.Instance.AudioActiveMode = 1;
+            audioListener.enabled = true;
+        }
+    }
+}
---
+++ Assets/EasyAR/Scripts/PrefsManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using MonsterAR.Utility;
+
+public class PrefsManager : Singleton<PrefsManager>
+{
+    public int AudioActiveMode {
+        get { return PlayerPrefs.GetInt("AudioActiveMode",1); }
+        set { PlayerPrefs.SetInt("AudioActiveMode",value); }
+    }
+}
