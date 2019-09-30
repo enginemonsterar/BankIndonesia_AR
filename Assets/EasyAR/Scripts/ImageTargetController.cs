5f1c9a70c754b918f0599cd0b33a16ebf8f68d43
Januar Elsan <januarelsan@gmail.com>
2019-09-30 17:08:35 +0700

    Create Full Screen Video

--- Assets/EasyAR/Scripts/ImageTargetController.cs
+++ Assets/EasyAR/Scripts/ImageTargetController.cs
@@ -227,10 +227,13 @@ public class ImageTargetController : MonoBehaviour
         ARModeManager.Instance.SetARMateri(markerIndex);
         if(isDynamic){
             gameObject.SetActive(true);
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                transform.GetChild(i).gameObject.SetActive(true);
-            }
+            
+            transform.GetChild(0).gameObject.SetActive(true);
+            
+            // for (int i = 0; i < transform.childCount; i++)
+            // {
+            //     transform.GetChild(i).gameObject.SetActive(true);
+            // }
         }else{
             
         }
---
+++ Assets/_Main/Scripts/VideoManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class VideoManager : MonoBehaviour
+{
+    [SerializeField] private GameObject canvasFooter;
+
+    [SerializeField] private GameObject closeButton;    
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private Camera targetCamera;
+
+    [SerializeField] private MeshRenderer targetMaterialRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeToFullScreen(){
+        videoPlayer.renderMode = VideoRenderMode.CameraNearPlane;
+        videoPlayer.targetCamera = this.targetCamera;
+        videoPlayer.aspectRatio = VideoAspectRatio.Stretch;
+
+        closeButton.SetActive(true);
+
+        canvasFooter.SetActive(false);
+    }
+
+    public void CloseFullScreen(){
+        videoPlayer.renderMode = VideoRenderMode.MaterialOverride;
+        videoPlayer.targetMaterialRenderer = this.targetMaterialRenderer;
+        
+
+        closeButton.SetActive(false);
+
+        canvasFooter.SetActive(true);
+    }
+}
