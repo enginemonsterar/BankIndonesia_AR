e584b57b05994f5dc3e95858e0e1dbf143bdabfe
Januar Elsan <januarelsan@gmail.com>
2019-10-10 10:55:21 +0700

    Change Map and Create Share Feature

---
+++ Assets/_Main/Scripts/Managers/ShareManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using VoxelBusters.NativePlugins;
+using VoxelBusters.Utility;
+using VoxelBusters.NativePlugins.Demo;
+
+public class ShareManager : MonoBehaviour
+{
+    private string m_shareImageRelativePath = "LogoShare.png";
+    // Start is called before the first frame update
+    
+    public void Share(){
+        ShareImageOnSocialNetwork ();
+    }
+
+    private void ShareImageOnSocialNetwork ()
+    {
+        // Create share sheet
+        SocialShareSheet _shareSheet 	= new SocialShareSheet();	
+        _shareSheet.Text				= "Ayo Main ke Musium Bank Indonesia";
+        _shareSheet.AttachImageAtPath(GetImageFullPath());
+        
+        // Show composer
+        NPBinding.UI.SetPopoverPointAtLastTouchPosition();
+        NPBinding.Sharing.ShowView(_shareSheet, FinishedSharing);
+    }
+
+    private void FinishedSharing (eShareResult _result)
+    {
+        // AddNewResult("Finished sharing");
+        // AppendResult("Share Result = " + _result);
+    }
+
+    private string GetImageFullPath ()
+    {
+        return Application.streamingAssetsPath + "/" + m_shareImageRelativePath;
+    }
+    
+}
